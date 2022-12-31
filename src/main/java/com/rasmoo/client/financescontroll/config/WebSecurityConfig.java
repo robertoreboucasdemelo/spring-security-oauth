2d4e5ef630d66a56a2c65998813bab3fee1e36fe
Roberto Reboucas de Melo <roberto.reboucas@ibm.com>
2022-12-31 00:55:39 -0300

    Add Grant Type Password

--- src/main/java/com/rasmoo/client/financescontroll/config/WebSecurityConfig.java
+++ src/main/java/com/rasmoo/client/financescontroll/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.rasmoo.client.financescontroll.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,6 +15,12 @@ import com.rasmoo.client.financescontroll.v1.service.UserInfoService;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception{
+		return super.authenticationManager();
+	}
 	
 	@Bean
 	public PasswordEncoder encorder() {
--- src/main/java/com/rasmoo/client/financescontroll/oauth/OauthConfiguration.java
+++ src/main/java/com/rasmoo/client/financescontroll/oauth/OauthConfiguration.java
@@ -1,16 +1,23 @@
 package com.rasmoo.client.financescontroll.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 public class OauthConfiguration {
 	public static final String RESOURCE_ID = "FinancesControll";
 	
+	@EnableResourceServer
 	public static class ResourceServer extends ResourceServerConfigurerAdapter{
 		
 		@Override
@@ -29,12 +36,29 @@ public class OauthConfiguration {
 		}
 	}
 	
+	@EnableAuthorizationServer
 	public static class AuthorizationServer extends AuthorizationServerConfigurerAdapter{
+		
+		@Autowired
+		private AuthenticationManager authenticationManager;
+		
+		@Autowired
+		private PasswordEncoder passwordEncoder;
+		
+		@Override
+		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+			endpoints.authenticationManager(authenticationManager);
+		}
+		
+		
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception{
 			clients.inMemory()
 			.withClient("cliente-web")
-			.secret("rasmoo")
+			.secret(passwordEncoder.encode("rasmoo"))
+			.authorizedGrantTypes("password")
+			.scopes("read", "write")
+			.accessTokenValiditySeconds(3601)
 			.resourceIds(RESOURCE_ID);
 		}
 	}
